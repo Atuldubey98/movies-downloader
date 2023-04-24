df9823fff3077b2549a2d959142cdb0d41bb7cae
Atuldubey98 <atuldubey017@gmail.com>
2023-04-24 16:18:36 +0530

    .env

--- movie-suggest/src/instance.tsx
+++ movie-suggest/src/instance.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
+  baseURL: import.meta.env.VITE_API_URL,
   params: {
-    api_key: "747e4d8790bd111419e88605ca28440c",
+    api_key: import.meta.env.VITE_API_KEY,
   },
 });
 
@@ -11,6 +11,6 @@ export const torrentInstance = axios.create({
   baseURL: import.meta.env.DEV ? "http//localhost:8080" : "/",
 });
 export default instance;
-export const imageUrl = "https://image.tmdb.org/t/p/original/";
+export const imageUrl = import.meta.env.VITE_IMAGE_URL;
 export const searchUrl = "/search/multi?include_adult=false";
 export const torrentSearchUrl = "/api/v1/all/search";
