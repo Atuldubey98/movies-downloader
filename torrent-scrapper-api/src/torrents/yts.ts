d13ad2661fdc0f762550b7aa59a8fefec7d5bfed
Atuldubey98 <atuldubey017@gmail.com>
2023-04-29 16:11:27 +0530

    commit

--- torrent-scrapper-api/src/torrents/yts.ts
+++ torrent-scrapper-api/src/torrents/yts.ts
@@ -9,7 +9,7 @@ class Yts extends TorrentSupper {
   }
   public async generateSearch(
     search: string,
-    page = 1
+    _: number
   ): Promise<ITorrentMovie[]> {
     const $: CheerioAPI = await super.getPageContent(
       `/browse-movies/${search}/all/all/1/latest/0/all`
