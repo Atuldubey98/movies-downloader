18db9d718d1f6d55ea7020807828d81ec554aa55
Atuldubey98 <atuldubey017@gmail.com>
2023-05-03 16:18:14 +0530

    server

--- torrent-scrapper-api/src/server.ts
+++ torrent-scrapper-api/src/server.ts
@@ -1,8 +1,16 @@
 import app from "./app";
+import cluster from "cluster";
+import os from "os";
 import { createServer } from "http";
-const server = createServer(app);
 const PORT: number = Number(process.env.PORT) || 8080;
 
-server.listen(PORT, () => {
-  console.log("App is running on ", PORT);
-});
+if (cluster.isPrimary) {
+  const noOfCpus = os.cpus().length;
+  for (let i = 0; i < noOfCpus; i++) cluster.fork();
+} else {
+  const server = createServer(app);
+  const pid = process.pid;
+  server.listen(PORT, () => {
+    console.log("App is running on ", PORT, "for pid ", pid);
+  });
+}
